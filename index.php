b99fcf1ed033e533dc4454733362e108c62bd805
GUILHERME ROJAS RIBEIRO <u17182@academico.cotuca.unicamp.br>
2018-09-24 11:25:47 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		kkanvas
+	</title>
+</head>
+<body style="font-size: 40px">
+	<a href="color.html">Colorido</a><br>
+	<a href="pong.html">Pong</a><br>
+	<a href="fractree.html">Fract Tree (WIP)</a><br>	
+	<a href="trippy.html">This?</a><br>
+	<a href="trippyOrig.html">That?</a><br>
+</body>
+</html>
